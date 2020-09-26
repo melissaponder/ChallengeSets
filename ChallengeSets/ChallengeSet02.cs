0ccfe5c710f62f17d5474aa985c079fdbc486673
Melissa Ponder <mponder71186@gmail.com>
2020-09-25 19:22:30 -0500

    Completed IsNumberOdd.

--- ChallengeSets/ChallengeSet02.cs
+++ ChallengeSets/ChallengeSet02.cs
@@ -38,7 +38,11 @@ namespace ChallengeSets
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            if (num % 2 != 0)
+            {
+                return true;
+            }
+            else return false;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
